14ac920410ea9a15d0e77efeb22ba901415e753d
Cali Castle <cali@calicastle.com>
2020-04-09 22:35:48 -0700

    Add test code

--- api/generate.ts
+++ api/generate.ts
@@ -1,6 +1,22 @@
+import { text } from 'micro'
+import { parse } from 'querystring'
 import { NowRequest, NowResponse } from '@now/node'
 
-export default (request: NowRequest, response: NowResponse) => {
-    const { name = 'World' } = request.query
-    response.status(200).send(`Hello ${name}!`)
+export default async (req: NowRequest, res: NowResponse) => {
+  const body = parse(await text(req))
+  let result, attachments
+
+  try {
+    result = body.text
+  } catch (error) {
+    result = error.message
+    attachments = [{ text: error.stack }]
+  }
+
+  const message = '\`' + body.text + '\`: ' + result
+  const response_type = 'in_channel'
+
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+
+  res.end(JSON.stringify({ response_type, text: message, attachments }))
 }
